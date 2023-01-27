c785a33484a3ef03c2859ec3330f93b8e401a1ff
tony <b.mboya@alustudent.com>
2023-01-27 20:04:56 +0200

    Initial Project Structure

---
+++ lib/Screen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Component1 extends StatelessWidget {
+  const Component1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
